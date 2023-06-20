77c9c4ada4ffb64ec5f0466d71b2573d74ebf37a
Shaunté Symone <ShaunteSymone@gmail.com>
2023-06-20 14:55:01 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var footer = document.querySelector(".contact");
+footer.style.background = "#d7ddf3";
+
+var tagline = document.querySelector(".tagline");
+tagline.innerText = "May flowers all year long";
+
+var address = document.querySelector("address");
+address.style.lineHeight = "1.5em";
+address.style.fontSize = "1.75em";
+
+var everyDay = document.querySelector("h3");
+everyDay.innerHTML = "We're here for you <em>every day</em> of the week.";
